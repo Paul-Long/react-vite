1ae7b326bb0c05dc01ab80d0b80419cd8451dff5
Paul <baolong.hou@cmexpro.com>
2024-06-04 14:31:58 +0800

    feat: backup contract 11

--- packages-web3/sdk/utils.ts
+++ packages-web3/sdk/utils.ts
@@ -41,6 +41,12 @@ export function getFaucetConfigPda(marginIndex: number): any {
 export const PerpMarketMap: Record<number, string> = {
   0: 'C3t6wLgfhdXHWyUXsS2uhbFk9dRrmzz8o7h7yypDm4CD',
   1: '9NC8STwSvhu9jbWBbmTGjg8AeTj7BznmGmzoDPZLTKV2',
+  2: 'AA1dttSTNM4pz2zZmNLy5H5QnpA5eQdgRU7hArAG6K9d',
+  3: '6afQWRpDYeoiooNmJmyJ5m8cRydwDERiJYCVKbboTWcT',
+  4: '3d5HXfwWosovTNQ6DvYGmHa8ELWgnr6SVD7tvWq8MW7t',
+  5: 'ERTaJEtXKDJjwDhp2KytpxzRFTSd3PGbJjFevK7PhF2W',
+  6: 'BnamF9QGDkXnE8TTqni3XQfDdnMWTcYVGPKjVbw5e1WR',
+  7: 'u38WpxcNi2KkW85nyNKrsEyR2S7jzJSQZTn7TvFR7P8',
 };
 
 export function getPerpMarketPda(marketIndex: number): any {
@@ -50,7 +56,13 @@ export function getPerpMarketPda(marketIndex: number): any {
 export function getOraclePda(marketIndex: number): any {
   return {
     0: new PublicKey('9souYUSs5ZqypET9DJ3fnhjuidFcqCNzpE7aPxTrBYop'),
+    2: new PublicKey('9souYUSs5ZqypET9DJ3fnhjuidFcqCNzpE7aPxTrBYop'),
+    3: new PublicKey('9souYUSs5ZqypET9DJ3fnhjuidFcqCNzpE7aPxTrBYop'),
+    4: new PublicKey('9souYUSs5ZqypET9DJ3fnhjuidFcqCNzpE7aPxTrBYop'),
     1: new PublicKey('GPQexWdA6QG4r6Wb7SHXasZuBtfYDRt668RB7Yy4daVG'),
+    5: new PublicKey('GPQexWdA6QG4r6Wb7SHXasZuBtfYDRt668RB7Yy4daVG'),
+    6: new PublicKey('GPQexWdA6QG4r6Wb7SHXasZuBtfYDRt668RB7Yy4daVG'),
+    7: new PublicKey('GPQexWdA6QG4r6Wb7SHXasZuBtfYDRt668RB7Yy4daVG'),
   }[marketIndex];
 }
 
@@ -58,6 +70,12 @@ export function getObservationPda(marketIndex: number): any {
   return {
     0: new PublicKey('C7xExBPv9kavyTgRUqfSx9qCpPzD6QPvTGKY2aZbutKA'),
     1: new PublicKey('5oB3DKDq13dURY4ANuikFGFtsBKHH4cnaGcsbkPWc5S8'),
+    2: new PublicKey('4pL5XfhT3cLUQ197vyxMp91S6gBpsoUtMrtRufuLfqbR'),
+    3: new PublicKey('7MXScfuob1rzYBcUrffaqm33wJunaxaA62LQKVfod6JC'),
+    4: new PublicKey('4FNdyG3aFpc4VYeXcMNrqA6wJD3Pi7k6EZmrtAqBvDb1'),
+    5: new PublicKey('DZ3Bm5J4SojTEkbDFtpk7Ps6rF2LosYeLZiJ46nSigJk'),
+    6: new PublicKey('3zCMn7HVcdsKKs5xLysRr7AcwwseJa7e1fQfi6A6PAf6'),
+    7: new PublicKey('3mHxfcs7dXW3zHUWs96CkPpgeMQZyNpheyZnZ9W5fYY3'),
   }[marketIndex];
 }
 
@@ -65,18 +83,30 @@ export function getMarginIndexByMarketIndex(marketIndex: number): number {
   return {
     0: 0,
     1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
   }[marketIndex] as number;
 }
 
 export function getMarginIndexByMarketIndexV2(marketIndex: number): number {
   return {
     0: 1,
+    2: 1,
+    3: 1,
+    4: 1,
     1: 2,
+    5: 2,
+    6: 2,
+    7: 2,
   }[marketIndex] as number;
 }
 
 export function getAllPerpMarkets() {
-  return [0, 1].map((i) => ({
+  return [0, 1, 2, 3, 4, 5, 6, 7].map((i) => ({
     pubkey: getPerpMarketPda(i),
     isSigner: false,
     isWritable: true,
@@ -98,7 +128,7 @@ export function getAllOracles() {
 }
 
 export function getAllObservations() {
-  return [0, 1].map((i) => ({
+  return [0, 1, 2, 3, 4, 5, 6, 7].map((i) => ({
     pubkey: getObservationPda(i),
     isSigner: false,
     isWritable: true,
