02b11d7c93de240235a2ac12cef2db22aaeeb806
Paul <baolong.hou@cmexpro.com>
2024-06-09 21:21:38 +0800

    feature: lp tvl to usd

--- packages-trade/worker/subscription/mark-price.ts
+++ packages-trade/worker/subscription/mark-price.ts
@@ -2,7 +2,16 @@ import {MarkPriceSubTypes} from '@rx/const/subsription';
 import {sendToUi} from '../ui';
 import {TopicSubject} from './TopicSubject';
 
-const markPrice$ = new TopicSubject({Types: MarkPriceSubTypes});
+const markPrice$ = new TopicSubject({
+  Types: MarkPriceSubTypes,
+  matchTopic: (topic1, topic2) => {
+    if (!topic1 || !topic2) {
+      return false;
+    }
+    const reg = new RegExp(/dc\.aps\.markprice\..*/);
+    return reg.test(topic2);
+  },
+});
 markPrice$.subscribe((o) => {
   sendToUi(o);
 });
