478c39118ce266da18a25c2192ecebe901ab0c68
Paul <baolong.hou@cmexpro.com>
2024-06-16 18:12:25 +0800

    feature: more wallet

--- packages-web3/context/WalletContext.tsx
+++ packages-web3/context/WalletContext.tsx
@@ -1,6 +1,7 @@
 import {RateClient} from '@/sdk';
 import {rateXClient$} from '@/streams/rate-x-client';
 import {BackpackWalletAdapter} from '@/wallets/BackpackWalletAdapter';
+import {OKXWalletAdapter} from '@/wallets/OKXWalletAdapter';
 import {useStream} from '@rx/hooks/use-stream';
 import {Adapter, WalletAdapterNetwork, WalletError} from '@solana/wallet-adapter-base';
 import {PhantomWalletAdapter} from '@solana/wallet-adapter-phantom';
@@ -25,7 +26,12 @@ export const WalletContextProvider: FC<{children: ReactNode}> = ({children}) => 
   );
 
   const wallets = useMemo(
-    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter(), new BackpackWalletAdapter()],
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new BackpackWalletAdapter(),
+      new OKXWalletAdapter(),
+    ],
     [network]
   );
 
--- packages-web3/hooks/use-sign-in.ts
+++ packages-web3/hooks/use-sign-in.ts
@@ -10,16 +10,36 @@ interface Params {
 }
 
 export function useSignIn(params: Params) {
-  const wallet = useWallet();
+  const {select, connect, signMessage, signIn, publicKey} = useWallet();
 
   useEffect(() => {
-    wallet.select(<WalletName>'Phantom');
-  }, [wallet]);
+    select(<WalletName>'Phantom');
+    // select(<WalletName>'OKX');
+    // select(<WalletName>'Backpack');
+    // select(<WalletName>'Solflare');
+    // connect().then();
+  }, [select, connect]);
 
   const onSignIn = useCallback(async () => {
     try {
-      await wallet.connect();
-      if (!wallet.signIn) {
+      // if (signMessage && publicKey) {
+      //   const message = new TextEncoder().encode(
+      //     'Welcome to RateX account: ' + publicKey.toBase58()
+      //   );
+      //   const sig: any = await signMessage(message);
+      //   // const isValid = await SignatureVerification.verify(sig.publicKey, message, signature);
+      //   const isValid = ed25519.verify(sig, message, publicKey.toBytes());
+      //   console.log('Is the signature valid?', isValid);
+      //   const result: SignResult = {
+      //     signature: bs58.encode(sig),
+      //     signedMessage: bs58.encode(message),
+      //     publicKey: bs58.encode(publicKey.toBytes()),
+      //   };
+      //   console.log(result);
+      //   params.onFinish?.(result);
+      //   return result;
+      // }
+      if (!signIn) {
         Toast.error('Wallet does not support Sign In With Solana!');
         return;
       }
@@ -27,19 +47,18 @@ export function useSignIn(params: Params) {
       const input: SolanaSignInInput = {
         statement: 'Welcome to RateX',
       };
-      const output = await wallet.signIn(input);
+      const output = await signIn(input);
 
       const result: SignResult = {
         signature: bs58.encode(output.signature),
         signedMessage: bs58.encode(output.signedMessage),
         publicKey: bs58.encode(output.account.publicKey),
       };
-      console.log('sign result: ', result);
       params.onFinish?.(result);
     } catch (e) {
       console.error('SignIn Error: ', e);
     }
-  }, [wallet]);
+  }, [signIn, signMessage, publicKey]);
 
   return {onSignIn};
 }
